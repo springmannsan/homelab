26e498f507157905f730d3953853d39de0db7b88
Sándor Springmann <sandorspringmann@Sandors-Mac-mini.local>
2025-11-21 11:08:41 +0100

    docker

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Starting setup script....."
 
+#setting up basic variables
+user = tail -n 1 /etc/passwd | cut -d: -f1
+echo "User $user found. Working with this user"
+
 # basic configurations
 
 echo "Creating folders...."
@@ -40,10 +44,6 @@ echo "  browsable = yes" >> /etc/samba/smb.conf
 service smbd restart
 ufw allow samba
 
-#get user
-user = tail -n 1 /etc/passwd | cut -d: -f1
-echo "User $user found. Provide password for this user"
-
 echo "Provide SMB password for user: $user"
 smbpasswd -a $user
 echo "Password set!"
@@ -52,7 +52,38 @@ echo "Password set!"
 cp ./scripts/backup.py /scripts
 chown root /scripts/backup.py
 chmod 750 /scripts/backup.py
-echo "0 4 * * * root /usr/bin/python3" >> /etc/crontab
+echo "0 4 * * * root /usr/bin/python3 /scripts/backup.py" >> /etc/crontab
+
+#docker
+
+#setup docker apt repository
+#https://docs.docker.com/engine/install/ubuntu/
+
+# Add Docker's official GPG key:
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+#install
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+
+#post installation steps
+groupadd docker
+usermod -aG docker $user
+newgrp docker
 
 #done 
 echo "OK"
