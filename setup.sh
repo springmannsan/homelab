a59d9665d53ff44e6f30f1265f57666a61e0ce2a
Sándor Springmann <sandorspringmann@Sandors-Mac-mini.local>
2025-11-21 17:46:30 +0100

    Logging

--- setup.sh
+++ setup.sh
@@ -21,21 +21,23 @@ mkdir /scripts
 chown root /scripts
 chmod 750 /scripts
 
+echo "Folders created"
+
 #apt update and upgrade
 
 echo "Updating repositories...."
-apt-get update 
+apt-get update >> log.out
 echo "Repos updated"
 echo "Upgrading..."
-apt-get -y upgrade
-echo "Upgraded..."
+apt-get -y upgrade >> log.out
+echo "Upgraded"
 
 # samba install and setup
 
 echo "Installing samba...."
-apt-get -y install samba
+apt-get -y install samba >> log.out
 echo "Samba installed..."
-echo "  [datashare]" >> /etc/samba/smb.conf
+echo "[datashare]" >> /etc/samba/smb.conf
 echo "  comment = Samaba on Ubuntu" >> /etc/samba/smb.conf
 echo "  path = /srv/data" >> /etc/samba/smb.conf
 echo "  read only = no" >> /etc/samba/smb.conf
@@ -47,20 +49,28 @@ ufw allow samba
 echo "Provide SMB password for user: $user"
 smbpasswd -a $user
 echo "Password set!"
+echo "Samba installed"
 
 # backup script
+
+echo "Creating backup job...."
+
 cp ./scripts/backup.py /scripts
 chown root /scripts/backup.py
 chmod 750 /scripts/backup.py
 echo "0 4 * * * root /usr/bin/python3 /scripts/backup.py" >> /etc/crontab
 
+echo "Backup job created"
+
 #docker
+
+echo "Installing docker..."
 
 #setup docker apt repository
 #https://docs.docker.com/engine/install/ubuntu/
 
 # Add Docker's official GPG key:
-apt install ca-certificates curl
+apt install ca-certificates curl >> log.out
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -74,10 +84,10 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-apt update
+apt update >> log.out
 
 #install
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >> log.out
 systemctl start docker
 
 #post installation steps
@@ -85,5 +95,4 @@ groupadd docker
 usermod -aG docker $user
 newgrp docker
 
-#done 
-echo "OK"
+echo "Docker installed"
